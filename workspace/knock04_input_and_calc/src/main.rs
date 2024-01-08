5649f3385cdf5670c39c7306accfe58a57939988
NalloChan <75219485+NalloChan@users.noreply.github.com>
2024-01-09 00:16:46 +0900

    multiple

---
+++ workspace/knock04_input_and_calc/src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).expect("Read error.");
+    let number: i32 = buffer.trim().parse().expect("Parse error.");
+    let multiple = 3;
+    let answer = number * multiple;
+    println!("{} * {} = {}", number, multiple, answer);
+}
