888d18b846c19948992a9efd71dc8a13411ad76c
NalloChan <75219485+NalloChan@users.noreply.github.com>
2024-01-11 02:06:56 +0900

    Add knock10_absolute_value crate

---
+++ workspace/knock10_absolute_value/src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).expect("Read error");
+    let number: i32 = buffer.trim().parse().expect("Parse error");
+
+    let absolute_value = number.abs();
+    println!("{}", absolute_value);
+}
