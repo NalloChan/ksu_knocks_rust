c4b05487e93064056a8e7bf71f752ada80bede0e
NalloChan <75219485+NalloChan@users.noreply.github.com>
2024-01-14 02:36:34 +0900

    Add knock18_array_input_initialize to Cargo.toml

---
+++ workspace/knock18_array_input_initialize/src/main.rs
@@ -0,0 +1,9 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).expect("Read error");
+    let number: i32 = buffer.trim().parse().expect("Parse error");
+    let array = [number; 10];
+    println!("{:?}", array);
+}
