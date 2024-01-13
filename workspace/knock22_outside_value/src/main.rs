da663b780e863972d438c038cbf558d88d346bf7
NalloChan <75219485+NalloChan@users.noreply.github.com>
2024-01-14 03:38:32 +0900

    Add knock22_outside_value to Cargo.toml (#24)

---
+++ workspace/knock22_outside_value/src/main.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).expect("Read error");
+    let number: i32 = buffer.trim().parse().expect("Parse error");
+
+    if number <= -10 || 10 <= number {
+        println!("OK");
+    }
+}
