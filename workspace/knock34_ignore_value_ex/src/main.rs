500a80acb2c1e963f227022c6fb5906c5970dc29
NalloChan <75219485+NalloChan@users.noreply.github.com>
2024-01-14 21:21:18 +0900

    Add knock34_ignore_value_ex to Cargo.toml

---
+++ workspace/knock34_ignore_value_ex/src/main.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).expect("Read error");
+    let number: i32 = buffer.trim().parse().expect("Parse error");
+
+    println!(
+        "{:?}",
+        (1..=9)
+            .filter(|x| *x != number && *x != number + 1)
+            .collect::<Vec<i32>>()
+    );
+}
